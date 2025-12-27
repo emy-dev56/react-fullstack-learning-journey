30f152d821be2c6f2a46477984d3258892e9626f
Emy <eg1712@gmail.com>
2025-12-27 03:45:47 +0200

    Display Todos

--- phase-1-react-basics/todoapp/ToDoApp/src/components/Todo.jsx
+++ phase-1-react-basics/todoapp/ToDoApp/src/components/Todo.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from 'react';
+import TodoItem from './TodoItem';
 
 export default function Todo() {
   const[todo, setTodo] = useState("");
+  const[todos, setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Add a task" value={todo} onChange={(e) => setTodo(e.target.value)} />
       <button type="submit">Add</button>
     </form>
+    {
+      todos.map((item, index) => (
+        <TodoItem key={index} item={item} />
+      ))
+    }
     </>
   )
 }
---
+++ phase-1-react-basics/todoapp/ToDoApp/src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TodoItem({item}) {
+  return (
+    <div>
+      <h3>{item}</h3>
+    </div>
+  )
+}
