c152c0c7c0fd56a5aba1db393a271b0f97606702
Emy <eg1712@gmail.com>
2025-12-27 04:20:38 +0200

    Style form

--- phase-1-react-basics/todoapp/ToDoApp/src/components/Form.jsx
+++ phase-1-react-basics/todoapp/ToDoApp/src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import style from "./Form.module.css";
 
 export default function Form({todos, setTodos}) {
   const[todo, setTodo] = useState("");
@@ -9,9 +10,11 @@ export default function Form({todos, setTodos}) {
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Add a task" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button type="submit">Add</button>
+      <form onSubmit={handleSubmit} className={style.todoform}>
+        <div className={style.container}>
+          <input type="text" placeholder="Add a task" value={todo} onChange={(e) => setTodo(e.target.value)} className={style.modernInput}/>
+          <button type="submit" className={style.modernButton}>Add</button>
+        </div>
       </form>
     </>
   );
