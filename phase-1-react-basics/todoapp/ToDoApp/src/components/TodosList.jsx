b7b623b68ea9a0b5ba31de35f7b5f030c56261df
Emy <eg1712@gmail.com>
2025-12-28 02:59:31 +0200

    Sort Todos

--- phase-1-react-basics/todoapp/ToDoApp/src/components/TodosList.jsx
+++ phase-1-react-basics/todoapp/ToDoApp/src/components/TodosList.jsx
@@ -2,9 +2,10 @@ import TodoItem from "./TodoItem";
 import style from "./TodosList.module.css";
 
 export default function TodosList({todos, setTodos}) {
+  const sortedTodos = todos.slice().sort((a, b) => a.done - b.done);
   return (
     <div className={style.list}>
-      {todos.map((item, index) => (
+      {sortedTodos.map((item, index) => (
         <TodoItem key={index} item={item} todos={todos} setTodos={setTodos} />
       ))}
     </div>
