7112f5763d9e11ae833196791390650f519e7fc9
Emy <eg1712@gmail.com>
2025-12-31 03:54:37 +0200

    Add Parent Components

--- phase-1-react-basics/FoodApp/src/App.jsx
+++ phase-1-react-basics/FoodApp/src/App.jsx
@@ -1,8 +1,10 @@
-import FoodList from "./components/FoodList";
-import Nav from "./components/Nav";
-import Search from "./components/Search"
-import { useState } from "react";
-import "./App.css";
+import FoodList from './components/FoodList';
+import Nav from './components/Nav';
+import Search from './components/Search';
+import { useState } from 'react';
+import Container from './components/Container';
+import InnerContainer from './components/InnerContainer';
+import './App.css';
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -10,9 +12,13 @@ function App() {
     <div className="App">
       <Nav />
       <Search setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} />
+        </InnerContainer>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
